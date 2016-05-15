51a45beb6ef4ca402e51bb55ae79b8bf9b4de530
mayeg <mayegarciadiaz@gmail.com>
2016-05-15 18:37:51 -0500

    + Conexión con servidor sandbox de base de datos.

--- integrador.py
+++ integrador.py
@@ -21,10 +21,10 @@ app.register_blueprint(mascota, url_prefix="/mascotas")
 app.register_blueprint(emergencia, url_prefix="/emergencia")
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'maye123'
-app.config['MYSQL_DATABASE_DB'] = 'integrador'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'ufps_34'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ufps_pp'
+app.config['MYSQL_DATABASE_DB'] = 'ufps_34'
+app.config['MYSQL_DATABASE_HOST'] = 'sandbox2.ufps.edu.co'
 mysql.init_app(app)
 
 if __name__ == "__main__":
